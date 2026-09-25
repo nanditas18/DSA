bb2a31dc8669d2847f8d1c254645bc3f9cc03a38
Nandita Singh <nanditasingh1709@gmail.com>
2026-09-25 11:35:52 +0530

    Sync GFG - Sum of Array (Basic)

---
+++ GFG/Basic/0_SumOfArray.java
@@ -0,0 +1,16 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/sum-all-array-elements/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+class Solution {
+	public int arraySum(int arr[]) {
+		int sum = 0;
+		for (int i = 0; i<arr.length; i++) {
+			sum += arr[i];
+		}
+		return sum;
+	}
+}
+
