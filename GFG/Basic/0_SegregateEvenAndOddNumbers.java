e21f66684dbb20c346fd929b5f0ac32d51e21b45
Nandita Singh <nanditasingh1709@gmail.com>
2026-09-25 11:37:02 +0530

    Sync GFG - Segregate Even and Odd numbers (Basic)

---
+++ GFG/Basic/0_SegregateEvenAndOddNumbers.java
@@ -0,0 +1,33 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/segregate-even-and-odd-numbers4629/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+import java.util.Arrays;
+
+class Solution {
+    void segregateEvenOdd(int arr[]) {
+        Arrays.sort(arr);
+
+        int n = arr.length;
+        int[] temp = new int[n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                temp[index++] = arr[i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 != 0) {
+                temp[index++] = arr[i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    }
+}
