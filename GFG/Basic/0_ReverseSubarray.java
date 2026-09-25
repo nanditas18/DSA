ca17944a0363617107b7500618d9b2c45ed7d201
Nandita Singh <nanditasingh1709@gmail.com>
2026-09-25 14:54:43 +0530

    Sync GFG - Reverse Subarray (Basic)

---
+++ GFG/Basic/0_ReverseSubarray.java
@@ -0,0 +1,21 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/reverse-sub-array5620/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+public class Solution {
+    public static ArrayList<Integer> reverseSubArray(ArrayList<Integer> arr, int l, int r) {
+        int left = l - 1;
+        int right = r - 1;
+        while (left < right) {
+            int temp = arr.get(left);
+            arr.set(left, arr.get(right));
+            arr.set(right, temp);
+            left++;
+            right--;
+        }
+        return arr; 
+    }
+}
+
